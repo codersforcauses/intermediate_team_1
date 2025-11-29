2f0e57f0c2c59ba569c03624b0391b91d1c88ac4
Ben Fitzhardinge <ben@fitzhardinge.net>
2025-11-29 11:48:23 +0800

    new use_uv

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -25,5 +25,16 @@ fi
 (cd server && uv sync)
 (cd client && npm install)
 
+# Run the database in the background
+docker compose up -d
+
+# Wait for the database to start
+# Get the container ID of the db service
+DB_CONTAINER_ID=$(docker compose ps -q db)
+echo "Waiting for the database to be ready..."
+until [ "$(docker inspect -f '{{.State.Health.Status}}' "$DB_CONTAINER_ID")" == "healthy" ]; do
+    sleep 1
+done
+
 # Nuke and migrate db
 (cd server && uv run ./nuke.sh)
