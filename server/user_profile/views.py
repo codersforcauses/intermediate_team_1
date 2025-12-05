e1d8a1234524cb5391444dc822bd0d28438e6539
belledw <belledw>
2025-12-05 16:54:56 +0800

    code from django workshop 29/11

---
+++ server/user_profile/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from user_profile.serializers import UserSerializer, ProfileSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User, Profile
+from rest_framework import generics, permissions
+
+# Create your views here.
+class UserList(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+class UserProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class UserProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
