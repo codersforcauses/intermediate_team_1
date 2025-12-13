455e37b931084546d6bfeee15501fbf47afd7cb9
iroha <iroha@RUI.>
2025-12-13 16:05:25 +0800

    making new client pages (404, about-us, home)

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,6 +6,7 @@ const isWindowsDevContainer = () =>
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    devIndicators: false,
   reactStrictMode: true,
   // dumb fix for windows docker
   webpack: isWindowsDevContainer()
