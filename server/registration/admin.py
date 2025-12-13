3da584437d1fe43c9ffb0b13149562736b81c7dd
Kerry Cao <kerrycao06@outlook.com>
2025-12-13 12:39:50 +0800

    Registering models in admin

--- server/registration/admin.py
+++ server/registration/admin.py
@@ -1,2 +1,9 @@
+from django.contrib import admin
+from .models import Household
+from .models import Business
+from .models import PetCarer
 
 # Register your models here.
+admin.site.register(Household)
+admin.site.register(Business)
+admin.site.register(PetCarer)
