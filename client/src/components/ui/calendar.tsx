62d7c7eabf63c79223a486ce05480a25b045ca83
Kerry Cao <kerrycao06@outlook.com>
2025-12-17 01:27:02 +0800

    Downloaded packages

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -40,7 +40,7 @@ function Calendar({
       )}
       captionLayout={captionLayout}
       formatters={{
-        formatMonthDropdown: (date) =>
+        formatMonthDropdown: (date) => 
           date.toLocaleString("default", { month: "short" }),
         ...formatters,
       }}
